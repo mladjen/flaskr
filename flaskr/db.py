23cb78f50229f3118f4e5c772b1276ffbc674ff7
mladjen <mladjen12@gmail.com>
2020-11-08 16:50:57 +1100

    adding most of the views

--- flaskr/db.py
+++ flaskr/db.py
@@ -23,7 +23,7 @@ def close_db(e=None):
         db.close()
 
 def init_db():
-    bg = get_db()
+    db= get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
@@ -34,3 +34,7 @@ def init_db_command():
     """ Clear the existing data and create new tables. """
     init_db()
     click.echo('Initializd the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
